4d367678e67512930903cc6da26dfbe6bf7dad99
din <danielforboy@gmail.com>
2021-03-29 11:21:54 +0800

    Update 2021-03-29T03:21:51.180Z

--- js/all.js
+++
@@ -1,2 +0,0 @@
-"use strict";!function(){var c;function t(){setTimeout(function(){$("#load").hide(),function t(o){var e=10<o/60?Math.floor(o/60):"0"+Math.floor(o/60);var n=10<o%60?o%60:"0"+o%60;$("#time").text("".concat(e,":").concat(n));o<=0?($("#time").text("重新載入"),clearTimeout(c)):c=setTimeout(function(){t(--o)},1e3)}(60)},1e3)}$(document).ready(function(){$("#load").show(),t(),$("#time").on("click",function(){$("#load").show(),clearTimeout(c),t()})})}();
-//# sourceMappingURL=all.js.map
