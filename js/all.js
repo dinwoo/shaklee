ec46c52bd4e4a1382dc10d11a9a7c555829d98a7
din <danielforboy@gmail.com>
2021-03-31 23:14:16 +0800

    Update 2021-03-31T15:14:10.823Z

---
+++ js/all.js
@@ -0,0 +1,2 @@
+"use strict";$(document).ready(function(){/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)||($("#QRCode").show(),$("#mainBody").hide())});
+//# sourceMappingURL=all.js.map
