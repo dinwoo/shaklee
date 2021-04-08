a83af52034df5e322cd5f319386c809b7475bb16
din <danielforboy@gmail.com>
2021-04-08 19:19:55 +0800

    Update 2021-04-08T11:19:44.335Z

--- js/all.js
+++ js/all.js
@@ -1,2 +1,2 @@
-"use strict";$(document).ready(function(){/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)||($("#QRCode").show(),$("#mainBody").hide())});
+"use strict";$(document).ready(function(){/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)?console.log("mobile"):($("#QRCode").show(),$("#mainBody").hide())});
 //# sourceMappingURL=all.js.map
